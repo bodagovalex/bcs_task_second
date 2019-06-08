8f0b7ab52a24754ca2e12b5aad30ab6838c9cb5b
Алексей Бодагов <bodagovalex@gmail.com>
2019-06-08 12:51:40 +0600

    Clear and formatting :) Successful Task

--- EFCodeFirstDLL/DataContext.cs
+++ EFCodeFirstDLL/DataContext.cs
@@ -1,9 +1,7 @@
 ﻿namespace EFCodeFirstDLL
 {
     using EF_CodeFirst.DataModel;
-    using System;
     using System.Data.Entity;
-    using System.Linq;
 
     public class DataContext : DbContext
     {
@@ -18,5 +16,4 @@
         public virtual DbSet<ArticleFirstType> ArticleFirstTypes { get; set; }
         public virtual DbSet<ArticleSecondType> ArticleSecondTypes { get; set; }
     }
-
 }
--- EFCodeFirstDLL/DataModel/ArticleFirstType.cs
+++ EFCodeFirstDLL/DataModel/ArticleFirstType.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EF_CodeFirst.DataModel
 {
@@ -19,7 +14,6 @@ namespace EF_CodeFirst.DataModel
         [Required]
         [StringLength(500)]
         public string Property { get; set; }
-        
         
         public virtual Article Article { get; set; }
     }
